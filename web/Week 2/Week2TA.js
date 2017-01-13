1958a7ad9987de0645fc9c9a0f185d2b965eb324
Joshua Judd <jud13013@byui.edu>
2017-01-13 14:55:58 -0700

    created some new folders to hold things. completely created my home website and stuff

---
+++ web/Week 2/Week2TA.js
@@ -0,0 +1,15 @@
+function makeKnown()
+{
+    alert("This button has been clicked yo!");
+}
+
+function changeColor()
+{
+    var input = $("#myColorStuffz").val();
+    $("#div1").css("background-color", input);
+}
+
+function hideSomething()
+{
+    
+}
