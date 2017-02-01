44a42e99b7be723eb02c4d6972404a3f016443a2
Joshua Judd <jud13013@byui.edu>
2017-01-31 21:20:24 -0700

    started the actual EJ HP

--- web/ElrathJourney/index.php
+++ web/ElrathJourney/index.php
@@ -1,9 +1,19 @@
+<?php
+
+
+
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Elrath's Journey Homepage</title>
+        <link rel="stylesheet" type="text/css"  href="elrath.css">
     </head>
     <body>
+        <header>
+            <h1>Elrath's Journey</h1>
+        </header>
         <h1>Hello World!</h1>
     </body>
 </html>
