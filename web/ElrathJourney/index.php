8ad1226f0ae90615e3ec729e4e160a9d04f7f832
Joshua Judd <jud13013@byui.edu>
2017-02-03 18:31:57 -0700

    Commented out background image

--- web/ElrathJourney/index.php
+++ web/ElrathJourney/index.php
@@ -38,18 +38,19 @@ $db = get_db();
     <body>
         <?php
          
-        $statement = $db->prepare("SELECT * FROM login");
-        $statement->execute();
+            $statement = $db->prepare("SELECT * FROM login");
+            $statement->execute();
         
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-	// The variable "row" now holds the complete record for that
-	// row, and we can access the different values based on their
-	// name
-	echo '<p>';
-	echo '<strong>' . $row['user_name'] . ' ' . $row['password'] . ':';
-	echo '</p>';
-}
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+            {
+                // The variable "row" now holds the complete record for that
+                // row, and we can access the different values based on their
+                // name
+                echo '<p>';
+                echo $row['user_name'] . ' ' . $row['password'] . ':';
+                echo '</p>';
+            }
+        
         ?>
     </body>
 </html>
