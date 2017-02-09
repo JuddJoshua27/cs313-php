2a5b05c0ae6cc853bdbf03ee6747c08eb4535fcd
Joshua Judd <jud13013@byui.edu>
2017-02-08 22:04:00 -0700

    fixing some things on the login page

--- web/ElrathJourney/dblogin.php
+++ web/ElrathJourney/dblogin.php
@@ -3,12 +3,22 @@ include "dbcontrol.php";
 
 $comm = get_db();
 
-echo "username and password";
+echo "username and password<br>";
 
-$username = $_POST['username'];
-$password = $_POST['pword'];
+$username = htmlspecialchars($_POST['username']);
+$password = htmlspecialchars($_POST['pword']);
 
-echo "the username is: $username";
+echo "the username is: $username <br>";
 echo "the password is: $password";
 
+$statement = $comm->prepare("SELECT user_name, password FROM login");
+$statement->execute();
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	
+	echo '<p>';
+	echo $row['user_name'] . ' - ' . $row['password'];
+	echo '</p>';
+}
 ?>
