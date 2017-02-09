c450ed8e0c4c9bd35987906b083d4ccf9a6251d3
Joshua Judd <jud13013@byui.edu>
2017-02-08 20:38:15 -0700

    creating the login page

--- web/ElrathJourney/login.php
+++ web/ElrathJourney/login.php
@@ -20,16 +20,19 @@
         </style>
     </head>
     <body>
+        <br><br><br><br><br><br><br><br><br><br>
         <p class="text">
             Please Log In<br>
             If returning please enter your username and password and click <span class="standout">Submit</span><br>
             If new, please enter your desired username and password and click <span class="standout">New Player</span> 
-        </p><br><br>
+        </p><br><br><br><br>
         <form class="centerAlign" action="dblogin.php" method="post">
             <label class="text">Name: </label>
             <input type="text" name="username" id="username" placeholder="Elrath the Hobbit"><br>
-            <label class="text">Name: </label>
-            <input type="password" name="pword" id="pword">
+            <label class="text">Password: </label>
+            <input type="password" name="pword" id="pword"><br>
+            <input type="submit" name="submit" id="submit" value="New Player">
+            <input type="submit" name="submit" id="submit" value="submit">
         </form>    
     </body>
 </html>
