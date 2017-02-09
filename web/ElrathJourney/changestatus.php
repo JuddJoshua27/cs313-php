cca212fb9628c3bcad2f8ead527e00ba89d9ef14
Joshua Judd <jud13013@byui.edu>
2017-02-09 11:02:53 -0700

    working out legistics of moving from login -> actually starting the game.

--- web/ElrathJourney/changestatus.php
+++ web/ElrathJourney/changestatus.php
@@ -17,9 +17,7 @@ if ($player_status == "current")
 $statement = $comm->prepare("SELECT * FROM player");
 $statement->execute();
 
-$rows = $statement->fetch(PDO::FETCH_ASSOC);
-
-foreach ($rows as $row)
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
     echo $row["status"] . " : Health:" . $row["health"] . ", Magic:" . $row["magic"] . ", Attack:" . $row["attack"] . ", Defence:" . $row["defence"] . ", Gold:" . $row["gold"] . "<br>";
 }
