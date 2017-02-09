70351106fdff241f199409dfb4e4cead56106f7e
Joshua Judd <jud13013@byui.edu>
2017-02-08 22:30:38 -0700

    fixing some things on the login page

--- web/ElrathJourney/dblogin.php
+++ web/ElrathJourney/dblogin.php
@@ -11,6 +11,18 @@ $password = htmlspecialchars($_POST['pword']);
 echo "the username is: $username <br>";
 echo "the password is: $password";
 
+$statement = $comm->prepare("SELECT user_name, password FROM login");
+$statement->execute();
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	if ($row['user_name'] == $username)
+    {
+        header("Location: gamestart.php");
+        die("Redirected to...");
+    }
+}
+
 $statement = $comm->prepare("INSERT INTO login(user_name, password) VALUES ('$username', '$password')");
 $statement->execute();
 
@@ -24,20 +36,4 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 	echo $row['user_name'] . ' - ' . $row['password'];
 	echo '</p>';
 }
-
-$statement = $comm->prepare("DELETE FROM login");
-$statement->execute();
-
-$statement = $comm->prepare("SELECT user_name, password FROM login");
-$statement->execute();
-
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-	
-	echo '<p>';
-	echo $row['user_name'] . ' - ' . $row['password'];
-	echo '</p>';
-}
-
-echo "things deleted";
 ?>
