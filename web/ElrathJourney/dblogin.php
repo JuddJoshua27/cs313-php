46b80d363d5f2e58df73947509b56ed223f0c559
Joshua Judd <jud13013@byui.edu>
2017-02-08 21:21:31 -0700

    fixing some things on the login page

--- web/ElrathJourney/dblogin.php
+++ web/ElrathJourney/dblogin.php
@@ -1,6 +1,11 @@
 <?php
-    include "dbcontrol.php";
+include "dbcontrol.php";
 
-    $comm = get_db();
+$comm = get_db();
+
+$username = _POST("username");
+$password = _POST("pword");
+
+echo $username . " : " . $password;
 
 ?>
