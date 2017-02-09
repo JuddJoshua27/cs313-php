0eef3c5a062592ec52e66a5b37d66c6120b86419
Joshua Judd <jud13013@byui.edu>
2017-02-08 22:50:36 -0700

    fixing some things on the login page

--- web/ElrathJourney/dblogin.php
+++ web/ElrathJourney/dblogin.php
@@ -6,39 +6,36 @@ $comm = get_db();
 echo "username and password<br>";
 
 $username = htmlspecialchars($_POST['username']);
-$password = htmlspecialchars($_POST['pword']);
 
 echo "the username is: $username <br>";
-echo "the password is: $password";
 
-$statement = $comm->prepare("SELECT user_name, password FROM login");
+$statement = $comm->prepare("SELECT user_name FROM login");
 $statement->execute();
 
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
 	if ($row['user_name'] == $username)
     {
-        if ($row['password'] == $password)
-        {
-            header("Location: gamestart.php");
-            die("Redirected to...");
-        }
+        header("Location: gamestart.php");
+        die("Redirected to...");
     }
 }
 
-$statement = $comm->prepare("INSERT INTO login(user_name, password) VALUES ('$username', '$password')");
+$statement = $comm->prepare("INSERT INTO login(user_name) VALUES ('$username'");
 $statement->execute();
 
-$statement = $comm->prepare("SELECT user_name, password FROM login");
+$statement = $comm->prepare("SELECT user_name FROM login");
 $statement->execute();
 
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
 	
 	echo '<p>';
-	echo $row['user_name'] . ' - ' . $row['password'];
+	echo $row['user_name'];
 	echo '</p>';
 }
+/*
 header("Location: gamestart.php");
 die("Redirected to...");
+*/
 ?>
