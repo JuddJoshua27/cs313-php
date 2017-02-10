90d500bce17a915764f741dff99f973e6dba04dd
Joshua Judd <jud13013@byui.edu>
2017-02-09 21:21:06 -0700

    creating the player and populating their inventory for the opening playthrough of the game.

--- web/ElrathJourney/page1.php
+++ web/ElrathJourney/page1.php
@@ -16,8 +16,8 @@
         </style>
     </head>
     <body>
-        <div style="display:inline; margin-right: 35%; border:3px solid white;">
-           <p>asdfasdfasdasdfasdfas dfasdf asdkfjha soidufn lakjbsdfguia sjkdbfli basdbfi abfidgjabsdglijbaslkjdbglkj abdfjkbg alkdfjbgiajdbflghalijdfnglkjabdfiu gbajifndgibajkdfkbgi abofdijgnklajdfgjbajkdfgihadfngljbadfbgkabldfjigjabdfghajlkd fngjlhialdfnglkja bdfjkgnajkbfgj nakjbfgiaberjbjabfkvbadlicgbiajbgliafdignadlkjfbgiuasdglhasildfjgnlajdfhgibadfljkghijladflgjkha  ashdgjnal jsdhfljkasnd fhsadkj lfsdahfjkasdnfi uhasdnf uliasd hf asldf lasd fjlkasjkdfh lashdfjaidbguoenbrgbqiurbgilhaebfviber9uqrg laskjdbfiuewbfijbibui</p>            
+        <div style="display:inline; margin-right: 35%;">
+           <p style="border:3px solid white;color:white;">asdfasdfasdasdfasdfas dfasdf asdkfjha soidufn lakjbsdfguia sjkdbfli basdbfi abfidgjabsdglijbaslkjdbglkj abdfjkbg alkdfjbgiajdbflghalijdfnglkjabdfiu gbajifndgibajkdfkbgi abofdijgnklajdfgjbajkdfgihadfngljbadfbgkabldfjigjabdfghajlkd fngjlhialdfnglkja bdfjkgnajkbfgj nakjbfgiaberjbjabfkvbadlicgbiajbgliafdignadlkjfbgiuasdglhasildfjgnlajdfhgibadfljkghijladflgjkha  ashdgjnal jsdhfljkasnd fhsadkj lfsdahfjkasdnfi uhasdnf uliasd hf asldf lasd fjlkasjkdfh lashdfjaidbguoenbrgbqiurbgilhaebfviber9uqrg laskjdbfiuewbfijbibui</p>            
         </div>
         
     </body>
