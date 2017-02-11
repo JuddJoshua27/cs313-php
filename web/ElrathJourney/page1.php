5de303efa3da9ee7727021fc93bfaf6927d4b66e
Joshua Judd <jud13013@byui.edu>
2017-02-11 11:40:27 -0700

    getting php to work hopefully

--- web/ElrathJourney/page1.php
+++ web/ElrathJourney/page1.php
@@ -68,8 +68,15 @@ $comm = get_db();
             $defence_manip += $row["defence_manip"];
         }
         
+        $total_health = $health + $health_manip;
+        $total_magic = $magic + $magic_manip;
+        $total_attack = $attack + $attack_manip;
+        $total_defence = $defence + $defence_manip;
+            
+        
         echo "<h3>" . $health_manip . " : " . $magic_manip . " : " . $attack_manip . " : " . $defence_manip . "</h3>";
         echo "<h3>" . $health . " : " . $magic . " : " . $attack . " : " . $defence . " : " . $gold . "</h3>";
+        echo "<h3>" . $total_health . " : " . $total_magic . " : " . $total_attack . " : " . $total_defence . " : " . $gold . "</h3>";
         ?>
     </body>
 </html>
