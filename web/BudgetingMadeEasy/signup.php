167af541174bdbc98462adc6a101d15f2d10b309
Joshua Judd <jud13013@byui.edu>
2017-02-16 22:07:30 -0700

    starting the budget idea

--- web/BudgetingMadeEasy/signup.php
+++ web/BudgetingMadeEasy/signup.php
@@ -29,19 +29,21 @@ session_start();
         </div>
         <div class="container-fluid">
             <div class="row">
+                
                 <div class="col-xs-3"></div>
+                
                 <div class="col-xs-6">
                     <form class="form-horizontal" method="post" action="createUser.php">
                         <div class="form-group">
                             <label class="control-label col-sm-2" for="username">Username:</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="username" placeholder="Enter Username">
+                                    <input type="text" class="form-control" id="username" name="username" placeholder="Enter Username">
                                 </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="pwd">Password:</label>
+                            <label class="control-label col-sm-2" for="password">Password:</label>
                                 <div class="col-sm-10"> 
-                                    <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
                                 </div>
                         </div>
                         <div class="form-group"> 
@@ -51,7 +53,9 @@ session_start();
                         </div>
                     </form>
                 </div>
+                
                 <div class="col-xs-3"></div>
+                
             </div>
         </div>
     </body>
