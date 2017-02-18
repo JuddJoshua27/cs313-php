21c53ca301ff4cd938db32d1ea49fe13360125b4
Joshua Judd <jud13013@byui.edu>
2017-02-17 19:58:29 -0700

    created pages 1-4, the left turn right off the bat.

--- web/ElrathJourney/resolve.php
+++ web/ElrathJourney/resolve.php
@@ -1,29 +1,15 @@
 <?php
 require "dbcontrol.php"
 $comm = get_db();
-/*
-$statement = $comm->prepare("SELECT * FROM player WHERE login_id = $userID");
-        $statement->execute();
-        
-        $player_id= null;
-        
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-        {
-            $player_id = $row["id"];
-        }
+
+$statement = $comm->prepare("DELETE FROM player_inventory");
+$statement->execute();
+$statement = $comm->prepare("DELETE FROM player");
+$statement->execute();
+$statement = $comm->prepare("DELETE FROM login");
+$statement->execute();
 
 session_start();
-$user_id = $_SESSION("userID");
-*/
-$player_id = 22;
-$statement = $comm->prepare("DELETE FROM player_inventory WHERE player_id = $player_id");
-$statement->execute();
-$statement = $comm->prepare("DELETE FROM player WHERE id = $player_id");
-$statement->execute();
-$statement = $comm->prepare("DELETE FROM login WHERE id = $user_id");
-$statement->execute();
-
-
 session_destroy();
 header('location: login.php');
 ?>
