5aa44d28c9f78e4eed49b149386d8a91adaae8a1
Joshua Judd <jud13013@byui.edu>
2017-02-17 20:00:21 -0700

    created pages 1-4, the left turn right off the bat.

--- web/ElrathJourney/resolve.php
+++ web/ElrathJourney/resolve.php
@@ -1,14 +1,14 @@
 <?php
 require "dbcontrol.php"
 $comm = get_db();
-
+/*
 $statement = $comm->prepare("DELETE FROM player_inventory");
 $statement->execute();
 $statement = $comm->prepare("DELETE FROM player");
 $statement->execute();
 $statement = $comm->prepare("DELETE FROM login");
 $statement->execute();
-
+*/
 session_start();
 session_destroy();
 header('location: login.php');
