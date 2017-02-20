dfd0e439faf120d4bb10fb2bc7d587b44aa2be3a
Joshua Judd <jud13013@byui.edu>
2017-02-20 13:50:45 -0700

    trying to fix the final things once the game is done

--- web/ElrathJourney/page18.php
+++ web/ElrathJourney/page18.php
@@ -124,6 +124,9 @@ $comm = get_db();
         
         $gold += 1000;
         
+        $statement = $comm->prepare("UPDATE player SET gold = $gold WHERE id = $_SESSION["playerID"]");
+        $statement->execute();
+        
         $statement = $comm->prepare("SELECT * FROM inventory i INNER JOIN player_inventory pi ON i.id = pi.inventory_id WHERE pi.player_id = $player_id; ");
         $statement->execute();
         
