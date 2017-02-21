81b8c889c6ba2e75c8a4898ee7cda5f933482fc0
Joshua Judd <jud13013@byui.edu>
2017-02-21 00:06:28 -0700

    working on budget stuff

--- web/BudgetingMadeEasy/createBudget.php
+++ web/BudgetingMadeEasy/createBudget.php
@@ -17,7 +17,12 @@ $AutoIns = $_POST["AInsurance"];
 $medical = $_POST["medical"];
 $MedIns = $_POST["MInsurance"];
 $phone = $_POST["phone"];
-$educations = $_POST["education"];
+$education = $_POST["education"];
 $other = $_POST["other"];
+
+$statement = $comm->prepare("INSERT INTO budget(church_donations, groceries, rent_mortgage, utilities, transportation, auto_insurance, medical_insurance, phone, education, medical, other_expenses, savings, income) VALUES($donations, $groceries, $rent, $utilities, $transport, $AutoIns, $MedIns, $phone, $educations, $medical, $other, $savings, $income)");
+$statement->execute();
+
+echo "Inserted Correctly! check the DB.";
 
 ?>
